806f05d47325267233a765a9fed368f4ba3eeec2
ibatuloff <s.ibatulov@yandex.ru>
2025-05-17 20:46:45 +0500

    add scrapy item pipeline

---
+++ newscraper/pipelines.py
@@ -0,0 +1,56 @@
+import psycopg2
+from scrapy.exceptions import DropItem
+from newscraper import settings
+from newscraper.logger import logger
+
+class DatabasePipeline:
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipe = cls()
+        pipe.conn_data = {
+            'host': crawler.settings.get('HOST1'),
+            'port': crawler.settings.get('DB_PORT'),
+            'dbname': crawler.settings.get('DB_NAME'),
+            'user': crawler.settings.get('DB_USER'),
+            'password': crawler.settings.get('DB_PASSWORD'),
+            'sslmode': 'verify-full',
+            'sslrootcert': crawler.settings.get('CA_PATH')
+        }
+        return pipe
+    def open_spider(self, spider):
+        self.conn = psycopg2.connect(**self.conn_data)
+        self.cur = self.conn.cursor()
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
+
+    def process_item(self, item, spider):
+        upsert_sql = """
+            INSERT INTO site (name, url)
+            VALUES (%s, %s)
+            ON CONFLICT (url) DO UPDATE SET name = EXCLUDED.name
+            RETURNING id;
+        """
+
+        insert_sql = """
+            INSERT INTO publication (title, text, url, site_id, published_at)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+
+        self.cur.execute(upsert_sql, (
+            spider.site_name, spider.site_url
+        ))
+        site_id = self.cur.fetchone()[0]
+
+        self.cur.execute(insert_sql, (
+            item.get('title'),
+            item.get('content'),
+            item.get('url'),
+            site_id,
+            int(item.get('date').timestamp()) * 1000 # конвертация в миллисекунды
+        ))
+
+        self.conn.commit()
+        return item
