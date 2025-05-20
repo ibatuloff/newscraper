13e3c2ffff928e9f69a255d11128846222e14164
Stepan <s.ibatulov@yandex.ru>
2025-05-20 22:11:54 +0500

    add spider to process

--- run_all.py
+++ run_all.py
@@ -2,6 +2,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from newscraper.spiders.kaspersky_spider import KasperskySpider
 from newscraper.spiders.angarasecurity_spider import AngaraSecuritySpider
+from newscraper.spiders.securityvision_spider import SecurityVesionSpider
 from dotenv import load_dotenv
 
 load_dotenv("/app/.env")
@@ -9,6 +10,7 @@ settings = get_project_settings()
 process = CrawlerProcess(settings)
 process.crawl(KasperskySpider)
 process.crawl(AngaraSecuritySpider)
+process.crawl(SecurityVesionSpider)
 process.start()
 
 
